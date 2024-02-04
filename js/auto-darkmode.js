1eb107aba9a3333fde1f563960668de5f7df5f90
湯遒之 <nauxscript@gmail.com>
2024-02-04 15:00:36 +0800

    feat: remove unused files

--- js/auto-darkmode.js
+++
@@ -1,8 +0,0 @@
-// darkmode
-document.addEventListener('DOMContentLoaded',function(){
-  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.querySelector('html').classList.add('dark')
-  } else {
-    document.querySelector('html').classList.remove('dark')
-  }
-})
--- js/index.js
+++
@@ -1,6 +0,0 @@
-window.addLoadEvent(() => {
-  if (localStorage.postList == 1) {
-    window.bindScroll(document.documentElement.clientHeight)
-    localStorage.postList = 0
-  }
-})
